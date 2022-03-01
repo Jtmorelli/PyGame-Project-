cd96a9287018373ba9eecb6d04f52cd10f181bda
Jake Morelli <jake.morelli98@gmail.com>
2022-02-28 23:39:57 -0600

    full screen changes

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,9 +16,9 @@ class AlienInvasion:
         self.settings = Settings()
 
         # set screen size
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings, screen_width = self.screen.get_rect().width
+        self.settings, screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         # set backrounf color
         self.bg_color = (230, 230, 230)
