92ea880015ef1487a5ce79496932d81cce50a647
Jake Morelli <jake.morelli98@gmail.com>
2022-02-28 22:44:37 -0600

    set backrounf color

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,30 +2,36 @@ import sys
 
 import pygame
 
+
 class AlienInvasion:
-    #Overall class to manage game assets and behavior
+    # Overall class to manage game assets and behavior
 
     def __init__(self):
-    #Initialized the game, and create game resources
+        # Initialized the game, and create game resources
         pygame.init()
 
-        #set screen size
+        # set screen size
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        # set backrounf color
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
-        #start the main loop for the game
+        # start the main loop for the game
         while True:
-            #Watch for ketboard and mouse events
+            # Watch for ketboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #Make the most recently drawn screen visible.
+            # redraw the screen during each pass though the loop
+            self.screen.fill(self.bg_color)
+
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
 
-if __name__ == '__main__':
-    #Make a game instance, and run the game
+
+if __name__ == "__main__":
+    # Make a game instance, and run the game
     ai = AlienInvasion()
     ai.run_game()
-
