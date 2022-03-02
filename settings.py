3876f77960623a51dc000ea6d2e02bb8143340d6
Jake Morelli <jake.morelli98@gmail.com>
2022-03-01 23:14:38 -0600

    settings changes

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     # a class to store all settings for alien invasion
 
     def __init__(self):
-        # initialize the games settings
+        # initialize the games static settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
@@ -19,7 +19,18 @@ class Settings:
         self.bullets_allowed = 3
 
         # alien settings
-        self.alien_speed = 1.0
         self.fleet_drop_speed = 10
-        # fleet_direction of 1 = right, -1 = left
+
+        # how quick game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        # initialize settings througout game
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet direction 1 = right -1 = left
         self.fleet_direction = 1
