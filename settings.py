e3da0f85df13de5b35512f1b41ae5902c4b0080e
Jake Morelli <jake.morelli98@gmail.com>
2022-03-01 23:16:02 -0600

    settings changes

--- settings.py
+++ settings.py
@@ -34,3 +34,9 @@ class Settings:
 
         # fleet direction 1 = right -1 = left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        # increase speed settings
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
