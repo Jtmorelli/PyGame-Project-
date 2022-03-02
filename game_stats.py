658de7a77439ec3cc379a707a83ae75f688dc0e6
Jake Morelli <jake.morelli98@gmail.com>
2022-03-01 22:19:27 -0600

    add game stats

---
+++ game_stats.py
@@ -0,0 +1,11 @@
+class GameStats:
+    # tract statistics for alien invasion
+
+    def __init__(self, ai_game):
+        # initialize stats
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        # initialized stats that can change furing game
+        self.ships_left = self.settings.ship_limit
